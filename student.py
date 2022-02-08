adde3a9ba3ed14699de36c917e2ca7e4c74aad6e
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-02-08 21:11:39 +0500

    task_1_1

--- student.py
+++ student.py
@@ -27,18 +27,20 @@ class Mentor:
             return 'Ошибка'
 
 class Lecturer(Mentor):
+    pass
 
 class Reviewer(Mentor):
+    pass
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+#
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
