dcc70461beca7b46e919dc679d23c3d0990fc26b
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-02-08 21:59:41 +0500

    lecturer_task

--- student.py
+++ student.py
@@ -1,3 +1,11 @@
+def avg(dictionary):
+    lst = dict.values(dictionary)
+    lst_unpacked = [x for l in lst for x in l]
+    value = sum(lst_unpacked)
+    numbers = len(lst_unpacked)
+    average = value/numbers
+    return average
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -47,6 +55,8 @@ class Lecturer(Mentor):
     def rate_hw(self):
         print('У лектора нет прав для выставления оценки')
 
+    def __str__(self):
+        print(f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg(self.grades)}')
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -58,8 +68,8 @@ class Reviewer(Mentor):
         print(f'Имя: {self.name}\nФамилия: {self.surname}')
 
 
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.courses_in_progress += ['Python']
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
 
 # cool_mentor = Mentor('Some', 'Buddy')
 # cool_mentor.courses_attached += ['Python']
@@ -68,13 +78,18 @@ class Reviewer(Mentor):
 # cool_mentor.rate_hw(best_student, 'Python', 10)
 # cool_mentor.rate_hw(best_student, 'Python', 10)
 
-# cool_lecturer = Lecturer('Peeta', 'Paka')
-# cool_lecturer.courses_attached += ['Python']
-# best_student.rate_lecture(cool_lecturer, 'Python', 7)
-#
+cool_lecturer = Lecturer('Peeta', 'Paka')
+cool_lecturer.courses_attached += ['Python']
+best_student.rate_lecture(cool_lecturer, 'Python', 7)
+best_student.rate_lecture(cool_lecturer, 'Python', 3)
+best_student.rate_lecture(cool_lecturer, 'Java', 5)
+
 # # print(best_student.grades)
 # print(cool_lecturer.grades)
 # cool_lecturer.rate_hw()
 
-some_reviewer = Reviewer('Bo', 'Bobkinson')
-print(some_reviewer)
+# some_reviewer = Reviewer('Bo', 'Bobkinson')
+# print(some_reviewer)
+
+# print(dict.values(cool_lecturer.grades))
+print(cool_lecturer)
