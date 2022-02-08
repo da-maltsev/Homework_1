0f65ae65b0781ca3132c4ac1302b820f4140c63a
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-02-08 21:07:07 +0500

    Task_2

--- student.py
+++ student.py
@@ -10,6 +10,16 @@ class Student:
     def add_courses(self, course_name):
         self.finished_course.append(course_name)
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and\
+        course in lecturer.courses_attached :
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -26,19 +36,36 @@ class Mentor:
         else:
             return 'Ошибка'
 
+
 class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+
+    def rate_hw(self):
+        print('У лектора нет прав для выставления оценки')
+
 
 class Reviewer(Mentor):
+    pass
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+cool_lecturer = Lecturer('Peeta', 'Paka')
+cool_lecturer.courses_attached += ['Python']
+best_student.rate_lecture(cool_lecturer, 'Python', 7)
+
+# print(best_student.grades)
+print(cool_lecturer.grades)
+cool_lecturer.rate_hw()
