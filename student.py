15485a07c5a145194c7a8b149712c9289b1bbf59
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-02-10 21:52:40 +0500

    i_wanna_die_after_this_task

--- student.py
+++ student.py
@@ -1,3 +1,4 @@
+from statistics import mean
 def avg(dictionary):
     lst = dict.values(dictionary)
     lst_unpacked = [x for l in lst for x in l]
@@ -79,6 +80,25 @@ class Reviewer(Mentor):
     def __str__(self):
         print(f'Имя: {self.name}\nФамилия: {self.surname}')
 
+def avg_grade_student(student_list, course):
+    grade = []
+    for student in student_list:
+        for key_, value_ in student.grades.items():
+            if key_ == course:
+                for grades in value_:
+                    grade.append(grades)
+    avg_grade = mean(grade)
+    return avg_grade
+
+def avg_grade_lecturer(lecturer_list, course):
+    grade = []
+    for lecturer in lecturer_list:
+        for key_, value_ in lecturer.grades.items():
+            if key_ == course:
+                for grades in value_:
+                    grade.append(grades)
+    avg_grade = mean(grade)
+    return avg_grade
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -92,14 +112,16 @@ worst_student.finished_course += ['Введение в программиров�
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
+fool_reviewer = Reviewer('Big', 'Smoke')
+fool_reviewer.courses_attached += ['Java']
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Java', 10)
+fool_reviewer.rate_hw(best_student, 'Java', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 cool_reviewer.rate_hw(worst_student, 'Python', 1)
 cool_reviewer.rate_hw(worst_student, 'Python', 5)
-cool_reviewer.rate_hw(worst_student, 'Java', 7)
+fool_reviewer.rate_hw(worst_student, 'Java', 7)
 
 cool_lecturer = Lecturer('Peeta', 'Paka')
 cool_lecturer.courses_attached += ['Python']
@@ -113,12 +135,14 @@ best_student.rate_lecture(fool_lecturer, 'Python', 4)
 best_student.rate_lecture(fool_lecturer, 'Python', 3)
 best_student.rate_lecture(fool_lecturer, 'Java', 5)
 
-# # print(best_student.grades)
-# print(cool_lecturer.grades)
-# cool_lecturer.rate_hw()
+print(best_student.grades)
+print(cool_lecturer.grades)
+cool_lecturer.rate_hw()
 
 # print(cool_reviewer)
 # print(cool_lecturer)
 # print(best_student)
 print(cool_lecturer>fool_lecturer)
 print(best_student>worst_student)
+print(avg_grade_student([best_student, worst_student], 'Java'))
+print(avg_grade_lecturer([cool_lecturer, fool_lecturer], 'Python'))
